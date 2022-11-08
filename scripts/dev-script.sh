57714b72bd04255f2d74061edf7156bd261ac7f0
Andy Lawless <72738714+andy-lawless@users.noreply.github.com>
2022-11-08 13:46:35 +0000

    Update dev-script.sh

--- scripts/dev-script.sh
+++ scripts/dev-script.sh
@@ -7,14 +7,14 @@ echo "FAMILY= " $FAMILY
 NAME=`aws ecs describe-task-definition --task-definition "${TASK_DEFINITION_NAME}" --region "${AWS_DEFAULT_REGION}" | jq .taskDefinition.containerDefinitions[].name`
 echo "NAME= " $NAME
 
-sed -i "s#BUILD_NUMBER#$IMAGE_TAG#g" task-definition.json
-sed -i "s#REPOSITORY_URI#$REPOSITORY_URI#g" task-definition.json
-sed -i "s#ROLE_ARN#$ROLE_ARN#g" task-definition.json
-sed -i "s#FAMILY#$FAMILY#g" task-definition.json
-sed -i "s#NAME#$NAME#g" task-definition.json
+sed -i "s#BUILD_NUMBER#$IMAGE_TAG#g" dev-task-definition.json
+sed -i "s#REPOSITORY_URI#$REPOSITORY_URI#g" dev-task-definition.json
+sed -i "s#ROLE_ARN#$ROLE_ARN#g" dev-task-definition.json
+sed -i "s#FAMILY#$FAMILY#g" dev-task-definition.json
+sed -i "s#NAME#$NAME#g" dev-task-definition.json
 
 
-aws ecs register-task-definition --cli-input-json file://task-definition.json --region="${AWS_DEFAULT_REGION}"
+aws ecs register-task-definition --cli-input-json file://dev-task-definition.json --region="${AWS_DEFAULT_REGION}"
 
 REVISION=`aws ecs describe-task-definition --task-definition "${TASK_DEFINITION_NAME}" --region "${AWS_DEFAULT_REGION}" | jq .taskDefinition.revision`
 echo "REVISION= " "${REVISION}"
